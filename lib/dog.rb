9819655b4dc90c8596195529a0ec8c3f1ebcdf5e
Alyssa Brown <alyssa.brown@live.com>
2019-04-02 00:35:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,12 @@
 class Dog 
   attr_accessor :name, :breed 
   attr_reader :id 
-  
+  @@all = []
   def initialize(id: nil, name:, breed:)
     @id = id 
     @name = name 
     @breed = breed 
+    @@all 
   end 
   
   def self.create_table 
